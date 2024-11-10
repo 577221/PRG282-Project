1e50ec5fc00df0c7daf5c2a321c50a65c9d67fae
ChrisvdW517 <christ.vdwesthuizen@gmail.com>
2024-11-10 18:37:53 +0200

    Add files via upload

---
+++ PRG282 Project New/PRG282 Project/Forms/Delete.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG282_Project
+{
+    
+    public partial class frmDelete : Form
+    {
+        MainForm mainForm;
+
+        public frmDelete(MainForm frm)
+        {
+            InitializeComponent();
+            mainForm = frm;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mainForm.Show();
+            this.Close();
+        }
+
+        private void frmDelete_Load(object sender, EventArgs e)
+        {
+            mainForm.Hide();
+        }
+
+        private void frmDelete_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mainForm.Show();
+        }
+    }
+}
---
+++ PRG282 Project New/PRG282 Project/Forms/Update.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG282_Project
+{
+    public partial class frmUpdate : Form
+    {
+        MainForm mainForm;
+        public frmUpdate(MainForm frm)
+        {
+            InitializeComponent();
+            mainForm = frm;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mainForm.Show();
+            this.Close();
+        }
+
+        private void Update_Load(object sender, EventArgs e)
+        {
+            mainForm.Hide();
+        }
+
+        private void frmUpdate_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mainForm.Show();
+        }
+    }
+}
